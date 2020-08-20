00147a0f605c3f9f510f6dd4bb3a64e6dcc6b5b3
Nnadozie Okeke <n.e.okeke@outlook.com>
2020-08-20 12:48:43 +0100

    challenge 4

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    <p className="App">
-      This was spun up using create-react-app. I've attempted to strip it down to the barest minimum needed for this mini-project.
+    <pre className="App">{`
+        This was spun up using create-react-app.
 
-      Navigate to src/index.js. Look at where it renders our App inside the element it finds by the id "root"
+        I've attempted to strip it down to the barest minimum needed for this mini-project.
 
-      Navigate to public/index.html. Find the element with id of root.
+        Navigate to src/index.js. Look at where it renders "App.js" as <App /> inside the element it finds by the id, "root".
 
-      Can you understand how this all links together?
-    </p>
+        Navigate to public/index.html. Find the element with id of root.
+
+        Can you understand how this all links together?
+        `}
+    </pre>
   );
 }
 
